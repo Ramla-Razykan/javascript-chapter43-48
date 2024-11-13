2895c7dffa422288061953034b42876a559e94c6
Ramla Eman <ramla.eman.ra@gmail.com>
2024-11-14 00:43:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// =================Question1: Show an alert box on click on a link. 
+// function showAlert() {
+//     alert("You clicked the link!");
+// }
+
+// =================Question2: Display some Mobile images in browser. On click on an image Show the message in alert to user. 
+// function showMessage() {
+//     alert("Thanks for purchasing a mobile from us");
+// }
+
+// =================Question3: Display 10 student records in table and each row should contain a delete button. If you click on a button to delete a record, entire row should be deleted. 
+// function deleteRow(button) {
+//     var row = button.parentElement.parentElement;
+//     row.remove();
+// }
+
+// =================Question4: Display an image in browser. Change the picture on mouseover and set the first picture on mouseout. 
+// function changeImage() {
+//     document.getElementById('myImage').src = 'image.jfif';
+// }
+// function resetImage() {
+//     document.getElementById('myImage').src = 'image2.jfif';
+// }
+
+// =================Question5: Show a counter in browser. Counter should increase on click on increase button and decrease on click on decrease button. And show updated counter value in browser
+// let counter = 0;
+// function increase() {
+//     counter++;
+//     document.getElementById('counterValue').textContent = counter;
+// }
+// function decrease() {
+//     counter--;
+//     document.getElementById('counterValue').textContent = counter;
+// }
